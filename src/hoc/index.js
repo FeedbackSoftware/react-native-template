96519e97217446855aed167900e15f0aa8eb7b0d
Edwin Vargas <edwinleandrovargas@gmail.com>
2018-10-05 10:45:49 -0400

    Agrega componentes de orden superior

---
+++ src/hoc/index.js
@@ -0,0 +1,2 @@
+export { default as withThrottle }        from './withThrottle';
+export { default as withConnectionAlert } from './withConnectionAlert';
---
+++ src/hoc/withThrottle.js
@@ -0,0 +1,33 @@
+import React, { PureComponent } from 'react';
+import { throttle } from 'lodash';
+
+const withThrottle = (WrappedComponent) => {
+
+  class PreventDoubleClick extends PureComponent {
+    componentDidMount() {
+      this.onPress = throttle(this.onPress.bind(this), 700,
+        {
+          leading: true,
+          trailing: false
+        });
+    }
+
+    componentWillUnmount() {
+      this.onPress.cancel();
+    }
+
+    onPress() {
+      this.props.onPress && this.props.onPress();
+    }
+
+    render() {
+      return <WrappedComponent {...this.props} onPress={this.onPress}/>;
+    }
+  }
+
+  PreventDoubleClick.displayName = `withThrottle(${WrappedComponent.displayName ||
+  WrappedComponent.name})`;
+  return PreventDoubleClick;
+};
+
+export default withThrottle;
