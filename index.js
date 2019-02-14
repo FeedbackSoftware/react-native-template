880381c49efdaef575a1411f3d0471ee9fd32747
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2019-02-14 15:27:48 -0400

    Start estructuring of the template

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 /** @format */
 
+import React               from 'react';
 import { AppRegistry }     from 'react-native';
 import App                 from './src';
 import { name as appName } from './app.json';
