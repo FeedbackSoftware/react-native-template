23bda453fc7af1f45873beb489b21364a0f7ae59
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2019-02-14 15:03:09 -0400

    Initial commit

--- index.js
+++ index.js
@@ -1,20 +1,20 @@
 /** @format */
 
-import {AppRegistry}         from 'react-native';
-import App from './src';
-import {name as appName}     from './app.json';
-import configureStore        from './src/state/store'
-import SplashScreen          from "react-native-splash-screen"
+import { AppRegistry }     from 'react-native';
+import App                 from './src';
+import { name as appName } from './app.json';
+// import configureStore        from './src/state/store'
+// import SplashScreen          from "react-native-splash-screen"
 
-export const initialState = {}
+// export const initialState = {};
 
 const Template = () => {
-  SplashScreen.hide();
+  // SplashScreen.hide();
 
-  const params = configureStore(initialState);
+  // const params = configureStore(initialState);
 
-  return <App {...params} />
-}
-
+  // return <App {...params} />
+  return <App />;
+};
 
 AppRegistry.registerComponent(appName, () => Template);
--- src/index.js
+++ src/index.js
@@ -1,26 +1,50 @@
-import React                 from 'react'
-import { Provider, connect } from 'react-redux'
-import { PersistGate }       from 'redux-persist/lib/integration/react'
-import { reduxifyNavigator } from 'react-navigation-redux-helpers'
-import AppNavigator          from './navigation'
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ * @lint-ignore-every XPLATJSCOPYRIGHT1
+ */
 
-const App = ({store, persistor}) => {
-  const AppNavigatorWithState = connect(
-    // Map state to props
-    ({ nav: state }) => ({
-      state
-    })
-  )(reduxifyNavigator(AppNavigator, 'root'))
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
 
-  return (
-    <Provider store={ store }>
-      <PersistGate
-        persistor={ persistor }
-      >
-        <AppNavigatorWithState />
-      </PersistGate>
-    </Provider>
-  )
+const instructions = Platform.select({
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
+  android:
+  'Double tap R on your keyboard to reload,\n' +
+  'Shake or press menu button for dev menu',
+});
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+        <View style={styles.container}>
+          <Text style={styles.welcome}>Welcome to React Native!</Text>
+          <Text style={styles.instructions}>To get started, edit App.js</Text>
+          <Text style={styles.instructions}>{instructions}</Text>
+        </View>
+    );
+  }
 }
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
