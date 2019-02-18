52c78bee81e5933de909d8a50d241112fecc626c
Nelson Zambrano <nelsonx.zambrano@gmail.com>
2019-02-18 15:43:09 -0400

    Ends configuring redux store for template

--- index.js
+++ index.js
@@ -4,18 +4,17 @@ import React               from 'react';
 import { AppRegistry }     from 'react-native';
 import App                 from './src';
 import { name as appName } from './app.json';
-// import configureStore        from './src/state/store'
+import configureStore      from './src/state/store'
 // import SplashScreen          from "react-native-splash-screen"
 
-// export const initialState = {};
+export const initialState = {}
 
 const Template = () => {
   // SplashScreen.hide();
 
-  // const params = configureStore(initialState);
+  const params = configureStore(initialState)
 
-  // return <App {...params} />
-  return <App />;
+  return <App { ...params } />
 };
 
 AppRegistry.registerComponent(appName, () => Template);
